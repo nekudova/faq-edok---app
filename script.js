06ccb5cef9af9fd7fd051bc5cc62838a0659f548
nekudova <nekudova@astorieas.cz>
2025-02-26 18:44:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.getElementById("faqForm").addEventListener("submit", function (event) {
     var question = document.getElementById("question").value;
     var status = document.getElementById("status");
 
-    fetch("https://script.google.com/macros/s/AKfycbzYf3HS1J5QkBqQuYxhjARRIN6MYcFCDCLCgEH-IEtBCJwfiXfsUeMKDRbbqeDObiLy/exec?question=" + encodeURIComponent(question), {
+    fetch("https://script.google.com/macros/s/AKfycbyObKReQTv0Jyds-e75NJCT3lPVToIxOQ7eOR2Gm5S8jerxZ5OQf7kQoGCHD0wYjQcy/exec" + encodeURIComponent(question), {
     method: "GET"
 })
    
