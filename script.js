c671ff36bc1bf627d685bc205a67b194949f15e8
nekudova <nekudova@astorieas.cz>
2025-02-26 18:23:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,31 +4,26 @@ document.addEventListener("DOMContentLoaded", function () {
     questions.forEach(question => {
         question.addEventListener("click", function () {
             const answer = this.nextElementSibling;
-            if (answer.style.display === "block") {
-                answer.style.display = "none";
-            } else {
-                answer.style.display = "block";
-            }
+            answer.style.display = (answer.style.display === "block") ? "none" : "block";
         });
     });
 });
-document.getElementById("faqForm").addEventListener("submit", function(event) {
+
+document.getElementById("faqForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     var question = document.getElementById("question").value;
     var status = document.getElementById("status");
 
-   fetch("https://script.google.com/macros/s/AKfycbw9yHkfHF0uM0eOThpD47W7Bu3ZqIfYqsrUFwgjn41JJcnMY995FHO3lVQP5Ptbw5Dk/exec", {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ question: question })
-    }).then(() => {
-        status.innerText = "Otázka byla odeslána!";
+    fetch("https://script.google.com/macros/s/AKfycbzlRlxA36AL_04qxFW9y5M9S73qzdWN7u4FB7OLZquyjJKhOXjN0mLLW91oPyAoVLuo/exec?question=" + encodeURIComponent(question), {
+        method: "GET"
+    })
+    .then(response => response.text())
+    .then(data => {
+        status.innerText = data;
         document.getElementById("faqForm").reset();
-    }).catch(error => {
+    })
+    .catch(error => {
         status.innerText = "Chyba při odesílání!";
     });
 });
