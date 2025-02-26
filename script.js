fb83b39a73a6d1b8d88fa8cefe330ae435808b02
nekudova <nekudova@astorieas.cz>
2025-02-26 11:40:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.getElementById("faqForm").addEventListener("submit", function(event) {
     var question = document.getElementById("question").value;
     var status = document.getElementById("status");
 
-    fetch("TVŮJ_GOOGLE_SCRIPT_URL", { // Sem vlož svůj Google Apps Script odkaz!
+    fetch("https://script.google.com/macros/s/AKfycbyg_YANNiTcmfUMllyLdrbYd3aMxH5kNeHl4F7mfBJVIfGAbHVAL_wKsZFDIEen1GOH/exec", { // Sem vlož svůj Google Apps Script odkaz!
         method: "POST",
         mode: "no-cors",
         headers: {
