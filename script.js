14d61bd112cf330d5f5b6ef0227aa3b28f86ec3e
nekudova <nekudova@astorieas.cz>
2025-02-27 08:20:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
         var question = document.getElementById("question").value;
         var status = document.getElementById("status");
 
+        if (!question.trim()) {
+            status.innerText = "Zadejte otázku!";
+            return;
+        }
+
         fetch("https://script.google.com/macros/s/AKfycbyObKReQTv0Jyds-e75NJCT3lPVToIxOQ7eOR2Gm5S8jerxZ5OQf7kQoGCHD0wYjQcy/exec", {  
             method: "POST",
             headers: {
