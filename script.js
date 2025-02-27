5ba04bef13f5b503a56bc56996aec9c431a9463f
nekudova <nekudova@astorieas.cz>
2025-02-27 09:20:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,25 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        fetch("https://script.google.com/macros/s/AKfycbxgCzcEUIbkTXip2DZWvFdPMzxb3sT8lIix41T1mYoM5oh2Uzp3_590NlbQDSPu5dJX/exec", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ question: question })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === "success") {
-                status.innerText = "Otázka byla úspěšně odeslána!";
-                document.getElementById("faqForm").reset();
-            } else {
-                status.innerText = "Chyba: " + data.message;
-            }
-        })
-        .catch(error => {
-            status.innerText = "Chyba při odeslání!";
-            console.error("Chyba:", error);
-        });
-    });
+ fetch("https://script.google.com/macros/s/AKfycbxWOdauNi1Zie5whFJmWR5rH7E2ceLo5P8Oef9E5jth0SBVYZLPx4fh1E4RVXWYQTre/exec", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ question: question })
+})
+.then(response => response.json())
+.then(data => {
+    if (data.status === "success") {
+        status.innerText = "Otázka byla úspěšně odeslána!";
+        document.getElementById("faqForm").reset();
+    } else {
+        status.innerText = "Chyba: " + data.message;
+    }
+})
+.catch(error => {
+    status.innerText = "Chyba při odeslání!";
+    console.error("Chyba:", error);
 });
